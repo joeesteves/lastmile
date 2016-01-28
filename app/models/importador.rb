7c45116c1f7396b3002cc87dd670b909b4a3093b
ponesteves <ajesteves@gmail.com>
2016-01-28 13:37:20 -0300

    refactor para excel

--- app/models/importador.rb
+++ app/models/importador.rb
@@ -1,13 +1,23 @@
+require 'roo'
 module Importador
 	extend ActiveSupport::Concern
 	module ClassMethods
 		def importar file, reporte
-			CSV.foreach(file.path, headers: true, header_converters: :symbol, col_sep: ';') do |row|
-				reg = row.to_hash.select do |k|
-					(self.column_names + self.instance_methods(false).map { |i| i.to_s.gsub('=','')}).map(&:to_sym).include? k
-				end
-				self.create! reg.merge({reporte: reporte})
+			case File.extname(file)
+			when '.csv' then csv file, reporte
 			end
 		end
+
+		private
+			def csv file, reporte
+				CSV.foreach(file.path, headers: true, header_converters: :symbol, col_sep: ';') do |row|
+					reg = row.to_hash.select do |k|
+						(self.column_names + self.instance_methods(false).map { |i| i.to_s.gsub('=','')}).map(&:to_sym).include? k
+					end
+					self.create! reg.merge({reporte: reporte})
+				end
+			end
+			
+
 	end
 end
--- app/models/reporteador.rb
+++ app/models/reporteador.rb
@@ -1,18 +1,20 @@
 class Reporteador
-  def initialize opts = {}
+  def initialize reporte, opts = {}
+    @reporte = 'fertilizadoras'
     @precio_gasoil = opts[:precio_gasoil] || 1
   end
 
-  def fertilizadoras
+  def generar
+    
     #obtengo hash k = nombre de la maquina y un v = un hash con superficie
-    hsh = Labor.where(reporte:'fertilizadoras').sup
+    hsh = Labor.where(reporte: @reporte).sup
     hsh.each do |k,v|
       hsh[k].merge!({horas: 0, gasoil: {cantidad: 0, costo: 0}, gastos_varios: 0})
     end
-    Mantenimiento.where(reporte:'fertilizadoras').horas.each do |k,v|
+    Mantenimiento.where(reporte: @reporte).horas.each do |k,v|
       hsh[k][:horas] = v
     end
-    Mantenimiento.where(reporte:'fertilizadoras').gasoil(@precio_gasoil).each do |k,v|
+    Mantenimiento.where(reporte: @reporte).gasoil(@precio_gasoil).each do |k,v|
       hsh[k][:gasoil] = v
     end
     Mantenimiento.gastos_varios.each do |k,v|
