c9aad1f2621076c63ff4e429372b7a891a260d1c
ponyesteves <ajesteves@gmail.com>
2016-02-02 15:02:08 -0300

    performance improvement

--- app/models/importador.rb
+++ app/models/importador.rb
@@ -10,35 +10,41 @@ module Importador
 		end
 
     def clean reporte
-      where(reporte: reporte).destroy_all
+      where(reporte: reporte).delete_all
     end
 
 		private
 			def csv file, reporte
-				CSV.foreach(file.path, headers: true, header_converters: :symbol, col_sep: ';') do |row|
-					reg = filter row.to_hash
-					self.create! reg.merge({reporte: reporte})
-				end
+        ActiveRecord::Base.transaction do
+  				CSV.foreach(file.path, headers: true, header_converters: :symbol, col_sep: ';') do |row|
+  					reg = filter row.to_hash
+            self.create! reg.merge({reporte: reporte})
+  				end
+        end
         true
-			end
+      end
 
 			def xlsx file, reporte
 				hoja = Roo::Excelx.new(file.path)
         header = hoja.row(1).map{ |h| h.downcase.to_sym}
-				(2..hoja.last_row).each do |i|
-    	 		reg = filter Hash[[header, hoja.row(i)].transpose]
-					self.create! reg.merge({reporte: reporte})
-				end
+        ActiveRecord::Base.transaction do
+    			(2..hoja.last_row).each do |i|
+      	 		reg = filter Hash[[header, hoja.row(i)].transpose]
+            self.create! reg.merge({reporte: reporte})
+          end
+        end
         true
 			end
 
 			def xls file, reporte
 				hoja = Roo::Excel.new(file.path)
 				header = hoja.row(1).map{ |h| h.downcase.to_sym}
-				(2..hoja.last_row).each do |i|
-					reg = filter Hash[[header, hoja.row(i)].transpose]
-					self.create! reg.merge({reporte: reporte})
-				end
+        ActiveRecord::Base.transaction do
+          (2..hoja.last_row).each do |i|
+  					reg = filter Hash[[header, hoja.row(i)].transpose]
+  					self.create! reg.merge({reporte: reporte})
+  				end
+        end
         true
 			end
 
